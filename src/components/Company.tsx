aa61a3431cdc4743c32cf07df6318527a50f35d9
adash333 <adash333@hotmail.com>
2026-06-11 00:16:55 +0900

    運営サイト一覧を更新

--- src/components/Company.tsx
+++ src/components/Company.tsx
@@ -22,7 +22,6 @@ export function Company() {
           <dd>
             <ul className="site-list">
               <li>子育てエビデンス相談室</li>
-              <li>WordPressでフリーオリジナルフォント</li>
               <li>サルでもわかるWEBプログラミング</li>
               <li>サルでもわかる機械学習</li>
             </ul>
